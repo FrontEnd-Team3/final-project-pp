6655bbd58b09ebf89964a290d301ad2098574c1f
seungyonggo <tmddyd432@gmail>
2023-07-11 23:17:31 +0900

    feat: alert, Chat CSS 수정

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,3 +1,6 @@
+import AlertMessage from "components/AlertMessege";
+import ChatList from "components/Chat/chat-list";
+import ConsumerChat from "components/Chat/consumer-chat";
 import ProductList from "components/ProductList";
 import RecentlyClicked from "components/RecentlyClicked/RecentlyClicked";
 
@@ -5,6 +8,9 @@ const Main = () => {
 	return (
 		<>
 			<ProductList />
+			<AlertMessage />
+			<ChatList />
+			<ConsumerChat />
 			<RecentlyClicked />
 		</>
 	);
