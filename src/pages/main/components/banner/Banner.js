ef0bd277bd158f09aa9d5472338abee77752a7f6
jeongwooHam <hamjw0122@gmail.com>
2023-07-13 16:37:38 +0900

    feat: 메인 페이지 배너 슬라이드

--- src/pages/main/components/banner/Banner.js
+++ src/pages/main/components/banner/Banner.js
@@ -1,17 +1,46 @@
 import styled from "styled-components";
 import FAKEIMG from "./images/banner.png";
+import FAKEIMG2 from "./images/banner2.png";
+import FAKEIMG3 from "./images/banner3.png";
 import { BsFillCircleFill } from "react-icons/bs";
+import { useEffect, useRef, useState } from "react";
 
 const Banner = () => {
+	const ImageArr = [FAKEIMG, FAKEIMG2, FAKEIMG3];
+
+	const slideRef = useRef(null);
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const IMAGE_SIZE = 860;
+	const SLIDE_RANGE = currentIndex * IMAGE_SIZE;
+
+	useEffect(() => {
+		slideRef.current.style.transition = "all 0.5s ease-in-out";
+		slideRef.current.style.transform = `translateX(-${SLIDE_RANGE}px)`;
+	}, [currentIndex]);
+
 	return (
 		<S.BannerWrapper>
-			<S.BannerContainer>
-				<S.BannerImg src={FAKEIMG} />
+			<S.BannerContainer ref={slideRef}>
+				{ImageArr.map(image => (
+					<S.BannerImg src={image} />
+				))}
 			</S.BannerContainer>
 			<S.BannerButtons>
-				<BsFillCircleFill size="15" color="white" />
-				<BsFillCircleFill size="15" color="white" />
-				<BsFillCircleFill size="15" color="white" />
+				<BsFillCircleFill
+					size="15"
+					color="white"
+					onClick={() => setCurrentIndex(0)}
+				/>
+				<BsFillCircleFill
+					size="15"
+					color="white"
+					onClick={() => setCurrentIndex(1)}
+				/>
+				<BsFillCircleFill
+					size="15"
+					color="white"
+					onClick={() => setCurrentIndex(2)}
+				/>
 			</S.BannerButtons>
 		</S.BannerWrapper>
 	);
@@ -23,16 +52,20 @@ const BannerWrapper = styled.div`
 	width: 860px;
 	margin: 50px auto;
 	position: relative;
+	overflow: hidden;
 `;
 
 const BannerContainer = styled.div`
-	width: 100%;
+	width: 860px;
+	display: flex;
 `;
 
 const BannerImg = styled.img`
-	width: 100%;
+	width: 860px;
+	height: 350px;
 	border: 3px solid black;
 	border-radius: 20px;
+	margin-right: 5px;
 `;
 
 const BannerButtons = styled.div`
