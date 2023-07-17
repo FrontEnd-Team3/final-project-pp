d7c5f255db5d4f9de63d80674614aebda04eea2d
jeongwooHam <hamjw0122@gmail.com>
2023-07-18 00:53:10 +0900

    feat: MSW with Mock Data
    Co-authored-by: Kim Yeseul <yesoryeseul@users.noreply.github.com>
    Co-authored-by: Jaewon <GrayHound0801@users.noreply.github.com>
    Co-authored-by: seungyonggo <seungyonggo@users.noreply.github.com>
    Co-authored-by: Heesuee Park <Sueddd@users.noreply.github.com>

---
+++ src/apis/chat.api.js
@@ -0,0 +1,8 @@
+// import { chatList } from "mock/chatList";
+// import { rest } from "msw";
+
+// export const getChatList = [
+// 	rest.get("/users", async (req, res, ctx) => {
+// 		return res(ctx.status(200), ctx.json(chatList));
+// 	}),
+// ];
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,11 @@
-import { rest } from "msw";
-export const handlers = [
-	rest.get("/users", async (req, res, ctx) => {
-		return res(ctx.json());
-	}),
+import { setupWorker } from "msw";
+// import * as productApi from "../apis/product.api";
+// import * as chatApi from "../apis/chat.api.js";
+import * as userApi from "../apis/user.api.js";
+
+const handlers = [
+	// ...Object.values(productApi),
+	// ...Object.values(chatApi),
+	...Object.values(userApi),
 ];
+export const worker = setupWorker(...handlers);
