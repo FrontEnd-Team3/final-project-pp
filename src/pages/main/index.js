cb74615fcdbcc55b40b4b471ed601fd99b30ef1c
jeongwooHam <hamjw0122@gmail.com>
2023-07-24 01:24:13 +0900

    fix: main products 로직 수정

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,35 +3,17 @@ import Banner from "./components/banner/Banner";
 import Slogan from "./components/slogan/Slogan";
 import styled from "styled-components";
 import RecentlyClicked from "components/RecentlyClicked";
-import { productList } from "mock/productsList";
-import MainProducts from "./components/products";
+import UsedProduct from "./components/used-product/UsedProduct";
+import FreeProduct from "./components/free-product/FreeProduct";
 
 const Main = () => {
-	const USEDPRODUCTS = productList?.filter(
-		product => product.status !== "판매완료" && product.price,
-	);
-
-	const FREEPRODUCTS = productList?.filter(
-		product => product.status !== "판매완료" && !product.price,
-	);
-
 	return (
 		<>
 			<Banner />
 			<Slogan />
-			{/* <UsedProduct /> */}
-			<MainProducts
-				title={"중고"}
-				list={USEDPRODUCTS}
-				url={"/used-transaction"}
-			/>
+			<UsedProduct />
 			<S.DivisionLine />
-			{/* <FreeProduct /> */}
-			<MainProducts
-				title={"무료"}
-				list={FREEPRODUCTS}
-				url={"/free-transaction"}
-			/>
+			<FreeProduct />
 			<RecentlyClicked />
 		</>
 	);
