7a6c18948c575f08a8307f2a953d424a48ca3445
jeongwooHam <hamjw0122@gmail.com>
2023-08-01 01:34:30 +0900

    feat: 메인 페이지 상품 목록에 강사님 데이터 연결 성공 (useQuery)

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,20 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Banner from "./components/Banner";
 import Slogan from "./components/Slogan";
 import styled from "styled-components";
 import RecentlyClicked from "components/RecentlyClicked";
 import UsedProduct from "./components/UsedProducts";
 import FreeProduct from "./components/FreeProducts";
-import TokenRepository from "repositories/TokenRepository";
 import ProductQueryApi from "apis/product.query.api";
 
 const Main = () => {
 	const { data } = ProductQueryApi.getProductList();
+	console.log("main", data);
 
-	useEffect(() => {
-		const token = TokenRepository.getToken();
-		console.log(token ? token : "null");
-	}, []);
+	// if (isLoading) return <Loading />;
 
 	return (
 		<>
