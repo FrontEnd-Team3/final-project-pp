2868e39b63b4d62ad75e6e4bbc1bcb71e1270eb7
heesuePark <phs8743@naver.com>
2023-08-15 21:36:51 +0900

    chore:파일오류 수정 & 반응형 구현중

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -191,26 +191,37 @@ const InfoWrapper = styled.div`
 	justify-content: space-between;
 	font-weight: bold;
 	font-size: 16px;
-	position: relative;
-	top: 10px;
-	right: 3px;
+	margin-top: 20px;
 	div:nth-child(3) {
 		color: ${({ theme }) => theme.PALETTE.darkPrimary};
 		position: relative;
 		bottom: 7px;
 	}
+	@media ${({ theme }) => theme.DEVICE.tablet} {
+		width: 150px;
+		margin-right: 40px;
+	}
 `;
 const Container = styled.div`
-	width: 100%;
+	width: 1060px;
+	margin: 0 auto;
 	border-bottom: 1px solid #dddddd;
 	padding-bottom: 20px;
+	@media ${({ theme }) => theme.DEVICE.tablet} {
+		width: 780px;
+	}
 `;
+
 const SideTitle = styled.div`
 	${LogoFont}
 	font-style: italic;
 	font-weight: bold;
 	font-size: 11px;
+	@media ${({ theme }) => theme.DEVICE.tablet} {
+		padding-left: 10px;
+	}
 `;
+
 const NavPage = styled.div`
 	cursor: pointer;
 	:hover {
@@ -228,20 +239,22 @@ const Category = styled.div`
 const Searchicon = styled.img`
 	width: 18px;
 	z-index: 1;
-	position: relative;
-	right: 14px;
 	cursor: pointer;
+	margin-left: -20px;
+	@media ${({ theme }) => theme.DEVICE.tablet} {
+		width: 15px;
+	}
 `;
 const SearchWrapper = styled.div`
-	position: relative;
-	top: 15px;
+	display: flex;
+	margin-top: 30px;
+	margin-left: 10px;
 `;
 
 const SearchBar = styled.input`
-	position: relative;
+	margin-top: 10px;
 	padding-bottom: 6px;
 	width: 450px;
-	left: 12px;
 	border: none;
 	:focus {
 		outline: none;
@@ -250,6 +263,11 @@ const SearchBar = styled.input`
 	::placeholder {
 		color: black;
 		font-size: 13px;
+	}
+	@media ${({ theme }) => theme.DEVICE.tablet} {
+		width: 320px;
+		margin-right: 30px;
+		margin-bottom: 10px;
 	}
 `;
 
@@ -265,10 +283,12 @@ const LogoWrapper = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	margin: 0 auto;
+	margin-top: 30px;
 	width: 1060px;
-	position: relative;
-	top: 20px;
+	margin-bottom: -20px;
+	@media ${({ theme }) => theme.DEVICE.tablet} {
+		width: 780px;
+	}
 `;
 
 const CategoryWrapper = styled.div`
