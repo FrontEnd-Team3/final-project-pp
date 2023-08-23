ed27be8d3ea1e131ec2373fa8f7efe541403342f
jeongwooHam <hamjw0122@gmail.com>
2023-08-23 22:49:38 +0900

    chore: dev와 합병

--- src/pages/main/components/Banner/index.js
+++ src/pages/main/components/Banner/index.js
@@ -1,9 +1,7 @@
 import FAKEIMG from "./images/banner.jpg";
-// import FAKEIMG2 from "./images/banner2.jpg";
-// import FAKEIMG3 from "./images/banner3.jpg";
+import FAKEIMG2 from "./images/banner2.jpg";
+import FAKEIMG3 from "./images/banner3.jpg";
 import FAKEIMG4 from "./images/banner4.jpg";
-import FAKEIMG5 from "./images/banner5.jpg";
-import FAKEIMG6 from "./images/banner6.jpg";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
@@ -12,7 +10,7 @@ import "swiper/css/pagination";
 import styled from "styled-components";
 
 const Banner = () => {
-	const ImageArr = [FAKEIMG, FAKEIMG4, FAKEIMG5, FAKEIMG6];
+	const ImageArr = [FAKEIMG, FAKEIMG2, FAKEIMG3, FAKEIMG4];
 
 	return (
 		<S.BannerContainer>
