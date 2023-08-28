9d84fc95b541c5839c8e905fec4282a00d3802c8
yesoryeseul <yesoryeseul07@gmail.com>
2023-08-28 20:03:38 +0900

    chore: 시세 count 로직 수정 및 주석 정리

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
 const useWindowSizeCustom = () => {
-	// 초기 state 값은 undefined로 세팅한다.
 	const [windowSize, setWindowSize] = useState({
 		width: undefined,
 		height: undefined,
@@ -12,20 +11,18 @@ const useWindowSizeCustom = () => {
 			const handleResize = () => {
 				if (window.innerWidth < 1200) {
 					setWindowSize({
-						// 현재 브라우저의 가로, 세로 길이로 셋팅
 						width: window.innerWidth * 0.9,
 						height: window.innerHeight * 0.8,
 					});
 				}
 			};
 
-			// resize 이벤트가 발생할 때 handleResize 함수가 실행되도록 한다.
+			// resize 이벤트가 발생할 때 handleResize 함수가 실행
 			window.addEventListener("resize", handleResize);
 
-			// 초기값을 설정할 수 있도록 handleResize 함수를 한 번 실행시킨다.
-			handleResize();
+			handleResize(); // 초기값 설정
 
-			// 이벤트 리스너를 제거하여 이벤트 리스너가 리사이즈될 때마다 계속해서 생겨나지 않도록 처리한다. (clean up)
+			// 이벤트 리스너를 제거하여 이벤트 리스너가 리사이즈될 때마다 계속해서 생겨나지 않도록 처리 (clean up)
 			return () => window.removeEventListener("resize", handleResize);
 		} else {
 			return () =>
@@ -33,7 +30,7 @@ const useWindowSizeCustom = () => {
 					return null;
 				});
 		}
-	}, []); // 컴포넌트가 처음 마운트 될때와 언마운트 될 때 실행
+	}, []);
 	return windowSize;
 };
 
